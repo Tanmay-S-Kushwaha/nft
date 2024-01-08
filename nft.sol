6f940a24be17038fe5860c4899cf175ca83dfd86
Tanmay-S-Kushwaha <139480136+Tanmay-S-Kushwaha@users.noreply.github.com>
2024-01-08 11:01:49 +0530

    Update nft.sol

--- nft.sol
+++ nft.sol
@@ -1,5 +1,18 @@
-{
-"name":"Pikachu Art",
-"description":"Pika pika pikachuuuu"
-"image":"ipfs://bafkreiao5yvesfpncfcscwabbscyfy63uvupvz7e24fq4nmm6snejoxd3m"
+// SPDX-License-Identifier: MIT
+
+pragma solidity 0.8.22;
+import "@openzeppelin/contracts/token/ERC721/extensions/ERC721URIStorage.sol";
+import "@openzeppelin/contracts/access/Ownable.sol";
+
+contract Pika is ERC721URIStorage, Ownable{
+    constructor(address initialOwner) ERC721('PikaNFTs', 'NFT') Ownable(initialOwner) {}
+
+    function mint (
+        address _to,
+        uint256 _tokenId,
+        string calldata _uri
+    ) external onlyOwner {
+        _mint(_to,_tokenId);
+        _setTokenURI(_tokenId,_uri);
+    }
 }
